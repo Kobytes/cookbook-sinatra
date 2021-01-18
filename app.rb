54dd963d02ca527f88cc068eeaf8d79981820a53
Thomas Marignan <marignanthomas10@gmail.com>
2021-01-18 15:38:33 +0100

    Sinatra X ngrok init and usable 🙌

--- app.rb
+++ app.rb
@@ -4,11 +4,24 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry-byebug'
 require 'better_errors'
+
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path(__dir__)
 end
 
 get '/' do
-  'Coming soon... 😏'
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end

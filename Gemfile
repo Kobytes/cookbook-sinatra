121fcc1dfa4d217ac41b0264bc3370a895482a77
Thomas Marignan <marignanthomas10@gmail.com>
2021-01-18 14:40:29 +0100

    Created sinatra app 🙌

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
 
-gem "sinatra"
-gem "sinatra-contrib"
-gem "pry-byebug"
-gem "better_errors"
-gem "binding_of_caller"
+source 'https://rubygems.org'
+
+gem 'better_errors'
+gem 'binding_of_caller'
+gem 'pry-byebug'
+gem 'sinatra'
+gem 'sinatra-contrib'
---
+++ app.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry-byebug'
+require 'better_errors'
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path(__dir__)
+end
+
+get '/' do
+  'Coming soon... 😏'
+end
